c30ae2e0a3b283bd96e64533ddefa09d12ab0896
Blackbaud-JonathanBell <jonathan.bell@blackbaud.com>
2018-05-09 12:51:19 -0400

    Update tests

--- src/shared/csrf-xhr.spec.ts
+++ src/shared/csrf-xhr.spec.ts
@@ -137,6 +137,37 @@ describe('Auth token integration', () => {
     });
   });
 
+  it('should not call csrf endpoint if bypassCsrf is set', (done) => {
+    const tokenPromise = BBCsrfXhr.request(
+      'https://example.com/token',
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      true
+    );
+    const tokenRequest = jasmine.Ajax.requests.mostRecent();
+
+    if (tokenRequest.url === 'https://example.com/token') {
+      tokenRequest.respondWith({
+        responseText: JSON.stringify({
+          access_token: 'xyz',
+          expires_in: 12345
+        }),
+        status: 200
+      });
+
+      tokenPromise.then((tokenResponse: any) => {
+        expect(tokenResponse).toEqual({
+          access_token: 'xyz',
+          expires_in: 12345
+        });
+
+        done();
+      });
+    }
+  });
+
   it('should not try to parse an empty response', () => {
     BBCsrfXhr.request('https://example.com/token');
     const request = jasmine.Ajax.requests.mostRecent();
