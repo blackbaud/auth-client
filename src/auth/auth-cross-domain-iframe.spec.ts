0e734c34ddc69e29833bb546c96866a930ffb32d
Bryon Wilkins <Bryon.Wilkins@blackbaud.com>
2019-03-12 17:11:59 -0400

    test getToken

--- src/auth/auth-cross-domain-iframe.spec.ts
+++ src/auth/auth-cross-domain-iframe.spec.ts
@@ -25,51 +25,68 @@ describe('Auth Cross Domain Iframe', () => {
     getTokenCalls = 0;
   });
 
-  it('creates a new frame if none exist', () => {
-    const getElementSpy = spyOn(document, 'getElementById').and.callThrough();
-    const requestSpy = spyOn(BBAuthDomUtility, 'addIframe');
+  describe('getToken', () => {
+    it('gets or creates an iframe then returns the token promise', () => {
+      const getOrMakeFrameSpy = spyOn(BBAuthCrossDomainIframe, 'getOrMakeIframe').and.returnValue(fakeIframe);
+      const getTokenFromIframeSpy = spyOn(BBAuthCrossDomainIframe, 'getTokenFromIframe')
+        .and.returnValue(new Promise(() => { return; }));
 
-    BBAuthCrossDomainIframe.getOrMakeIframe();
+      BBAuthCrossDomainIframe.getToken();
 
-    expect(getElementSpy).toHaveBeenCalledWith('auth-cross-domain-iframe');
-    expect(requestSpy).toHaveBeenCalledWith('', 'auth-cross-domain-iframe', '');
+      expect(getOrMakeFrameSpy).toHaveBeenCalled();
+      expect(getTokenFromIframeSpy).toHaveBeenCalledWith(fakeIframe);
+    });
   });
 
-  it('uses an existing frame if one exists', () => {
-    const getElementSpy = spyOn(document, 'getElementById').and.returnValue(fakeIframe);
-    const requestSpy = spyOn(BBAuthDomUtility, 'addIframe');
+  describe('getOrMakeIframe', () => {
+    it('creates a new frame if none exist', () => {
+      const getElementSpy = spyOn(document, 'getElementById').and.callThrough();
+      const requestSpy = spyOn(BBAuthDomUtility, 'addIframe');
 
-    BBAuthCrossDomainIframe.getOrMakeIframe();
+      BBAuthCrossDomainIframe.getOrMakeIframe();
 
-    expect(getElementSpy).toHaveBeenCalledWith('auth-cross-domain-iframe');
-    expect(requestSpy).not.toHaveBeenCalled();
+      expect(getElementSpy).toHaveBeenCalledWith('auth-cross-domain-iframe');
+      expect(requestSpy).toHaveBeenCalledWith('', 'auth-cross-domain-iframe', '');
+    });
+
+    it('uses an existing frame if one exists', () => {
+      const getElementSpy = spyOn(document, 'getElementById').and.returnValue(fakeIframe);
+      const requestSpy = spyOn(BBAuthDomUtility, 'addIframe');
+
+      BBAuthCrossDomainIframe.getOrMakeIframe();
+
+      expect(getElementSpy).toHaveBeenCalledWith('auth-cross-domain-iframe');
+      expect(requestSpy).not.toHaveBeenCalled();
+    });
   });
 
-  it('communicates with the iframe via "ready" and "getToken" and kicks off "ready"', (done) => {
-    fakeIframe = BBAuthDomUtility.addIframe('', 'auth-cross-domain-iframe', '');
-    IFrameMock(fakeIframe);
+  describe('getTokenFromIframe', () => {
+    it('communicates with the iframe via "ready" and "getToken" and kicks off "ready"', (done) => {
+      fakeIframe = BBAuthDomUtility.addIframe('', 'auth-cross-domain-iframe', '');
+      IFrameMock(fakeIframe);
 
-    BBAuthCrossDomainIframe.getTokenFromIframe(fakeIframe)
-      .then((tokenResonse) => {
-        expect(tokenResonse.access_token).toEqual('accessToken!');
-        expect(tokenResonse.expires_in).toEqual(0);
-        done();
-      });
-  });
+      BBAuthCrossDomainIframe.getTokenFromIframe(fakeIframe)
+        .then((tokenResonse) => {
+          expect(tokenResonse.access_token).toEqual('accessToken!');
+          expect(tokenResonse.expires_in).toEqual(0);
+          done();
+        });
+    });
 
-  it('only calls the iframe once if the getToken is called', (done) => {
-    fakeIframe = BBAuthDomUtility.addIframe('', 'auth-cross-domain-iframe', '');
-    IFrameMock(fakeIframe);
+    it('only calls the iframe once if the getToken is called', (done) => {
+      fakeIframe = BBAuthDomUtility.addIframe('', 'auth-cross-domain-iframe', '');
+      IFrameMock(fakeIframe);
 
-    BBAuthCrossDomainIframe.getTokenFromIframe(fakeIframe)
-      .then((tokenResonse) => {
-        BBAuthCrossDomainIframe.getTokenFromIframe(fakeIframe)
-          .then((tr) => {
-            expect(getTokenCalls).toEqual(2);
-            done();
-          });
+      BBAuthCrossDomainIframe.getTokenFromIframe(fakeIframe)
+        .then((tokenResonse) => {
+          BBAuthCrossDomainIframe.getTokenFromIframe(fakeIframe)
+            .then((tr) => {
+              expect(getTokenCalls).toEqual(2);
+              done();
+            });
 
-      });
+        });
+    });
   });
 
 });
