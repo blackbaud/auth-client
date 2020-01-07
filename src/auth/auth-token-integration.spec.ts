88d94ba7dce7351b021417b36a8a345e352d5d7f
Alexis Coulter <Alexis.Coulter@blackbaud.me>
2020-01-07 17:04:28 -0500

    Adding additional tests to bring code coverage back up

--- src/auth/auth-token-integration.spec.ts
+++ src/auth/auth-token-integration.spec.ts
@@ -22,6 +22,8 @@ describe('Auth token integration', () => {
   describe('when the location host name is blackbaud.com', () => {
     beforeEach(() => {
       requestSpy = spyOn(BBCsrfXhr, 'request');
+      spyOn(BBAuthGetDomain, 'isRegisteredDomain')
+      .and.returnValue(false);
       spyOn(BBAuthGetDomain, 'getSTSDomain')
         .and.returnValue('https://s21aidntoken00blkbapp01.nxt.blackbaud.com');
       spyOn(BBAuthTokenIntegration, 'getLocationHostname').and.returnValue('blackbaud.com');
@@ -73,10 +75,11 @@ describe('Auth token integration', () => {
 
   });
 
-  describe('when the host name location is not blackbaud.com', () => {
+  describe('when the host name location is not blackbaud.com or a registered third party', () => {
     beforeEach(() => {
       requestSpy = spyOn(BBAuthCrossDomainIframe, 'getToken');
-
+      spyOn(BBAuthGetDomain, 'isRegisteredDomain')
+      .and.returnValue(false);
       spyOn(BBAuthTokenIntegration, 'getLocationHostname').and.returnValue('forgoodfund.com');
     });
 
@@ -89,6 +92,61 @@ describe('Auth token integration', () => {
         leId: undefined,
         permissionScope: undefined
       });
+    });
+  });
+
+  describe('when the location host name is a registered third party', () => {
+    beforeEach(() => {
+      requestSpy = spyOn(BBCsrfXhr, 'request');
+      spyOn(BBAuthGetDomain, 'isRegisteredDomain')
+      .and.returnValue(true);
+      spyOn(BBAuthGetDomain, 'getSTSDomain')
+        .and.returnValue('https://myRegisteredThirdPartySTS.com');
+      spyOn(BBAuthTokenIntegration, 'getLocationHostname').and.returnValue('myRegisteredThirdParty.com');
+    });
+
+    it('should request a token without params', () => {
+      BBAuthTokenIntegration.getToken();
+
+      expect(requestSpy).toHaveBeenCalledWith(
+        'https://myRegisteredThirdPartySTS.com/oauth2/token',
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        undefined,
+        true
+      );
+
+    });
+
+    it('should request a token with envId and permissionScope', () => {
+      BBAuthTokenIntegration.getToken(true, 'abc', '123');
+
+      expect(requestSpy).toHaveBeenCalledWith(
+        'https://myRegisteredThirdPartySTS.com/oauth2/token',
+        undefined,
+        true,
+        'abc',
+        '123',
+        undefined,
+        true
+      );
+
+    });
+
+    it('should request a token with envId, permissionScope, and leId', () => {
+      BBAuthTokenIntegration.getToken(true, 'abc', '123', 'xyz');
+
+      expect(requestSpy).toHaveBeenCalledWith(
+        'https://myRegisteredThirdPartySTS.com/oauth2/token',
+        undefined,
+        true,
+        'abc',
+        '123',
+        'xyz',
+        true
+      );
     });
 
   });
