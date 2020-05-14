657dc92649fda33db711a106d06fb3392a2f636e
Paul Crowder <paul.crowder@blackbaud.com>
2020-05-14 15:57:45 -0400

    Fixed property order (#142)

--- src/auth/auth-domain.ts
+++ src/auth/auth-domain.ts
@@ -1,12 +1,12 @@
 const thirdPartyDomainSTSUrlMappings: { [domain: string]: string; } = {
   'bbk12.com': 'https://sts-sso.bbk12.com',
+  'blackbaudfaith.com': 'https://sts.blackbaudfaith.com',
   'donorcentral.com': 'https://sts-dc.donorcentral.com',
   'mycampus-app.com': 'https://sts-sso.mycampus-app.com',
   'myschoolapp.com': 'https://sts-sso.myschoolapp.com',
   'myschoolautomation.com': 'https://sts-sso.myschoolautomation.com',
   'myschooldemo.com': 'https://sts-sso.myschooldemo.com',
-  'myschooltraining.com': 'https://sts-sso.myschooltraining.com',
-  'blackbaudfaith.com': 'https://sts.blackbaudfaith.com'
+  'myschooltraining.com': 'https://sts-sso.myschooltraining.com'
 };
 const defaultSTSUrl = 'https://s21aidntoken00blkbapp01.nxt.blackbaud.com';
 
