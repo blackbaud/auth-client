49a4e3a35096add44063b32ff3de1fdfda230f46
Veeha Khanna <Veeha.Khanna@blackbaud.me>
2020-06-30 16:14:53 -0400

    fixed tslint issues

--- src/auth/auth-domain.ts
+++ src/auth/auth-domain.ts
@@ -1,15 +1,14 @@
 const thirdPartyDomainSTSUrlMappings: { [domain: string]: string; } = {
   'bbk12.com': 'https://sts-sso.bbk12.com',
   'blackbaudfaith.com': 'https://sts.blackbaudfaith.com',
+  'blackbaudhosting.com': 'https://sts.blackbaudhosting.com',
   'donorcentral.com': 'https://sts-dc.donorcentral.com',
+  'etapestry.com': 'https://sts.etapestry.com',
   'mycampus-app.com': 'https://sts-sso.mycampus-app.com',
   'myschoolapp.com': 'https://sts-sso.myschoolapp.com',
   'myschoolautomation.com': 'https://sts-sso.myschoolautomation.com',
   'myschooldemo.com': 'https://sts-sso.myschooldemo.com',
-  'myschooltraining.com': 'https://sts-sso.myschooltraining.com',
-  'etapestry.com': 'https://sts.etapestry.com',
-  'blackbaudhosting.com': 'https://sts.blackbaudhosting.com'
-
+  'myschooltraining.com': 'https://sts-sso.myschooltraining.com'
 };
 const defaultSTSUrl = 'https://s21aidntoken00blkbapp01.nxt.blackbaud.com';
 
